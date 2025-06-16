19607cdb3b650484670b820118892dee95eaac85
Mark <markfitz815@gmail.com>
2025-06-16 20:01:40 +0100

    Encapsulate logic more

--- src/robot-game.ts
+++ src/robot-game.ts
@@ -1,43 +1,91 @@
 function gridGame(grid: number[][]): number {
   // Can only move right or down
   const getOptimumPath = (grid): number => {
-    const firstRow = [...grid[0]];
-    const secondRow = [...grid[1]];
+    const firstRow: number[] = [...grid[0]];
+    const secondRow: number[] = [...grid[1]];
 
     const rowOnePotential: number[] = [];
     const rowTwoPotential: number[] = [];
 
-    let counter = 0;
+    const setPotentialArrays = () => {
+      let counter = 0;
 
-    [...firstRow].reverse().forEach((value) => {
-      counter += value;
-      rowOnePotential.unshift(counter);
-    });
+      [...firstRow].reverse().forEach((value) => {
+        counter += value;
+        rowOnePotential.unshift(counter);
+      });
 
-    counter = 0;
+      counter = 0;
 
-    [...secondRow].reverse().forEach((value) => {
-      counter += value;
-      rowTwoPotential.unshift(counter);
-    });
+      [...secondRow].reverse().forEach((value) => {
+        counter += value;
+        rowTwoPotential.unshift(counter);
+      });
+    };
 
-    console.log(rowOnePotential);
-    console.log(rowTwoPotential);
+    const smartNavigation = (overwriteValues = false) => {
+      let moveDownIndex = -1;
 
-    let moveDownIndex = -1;
-
-    for (let i = 0; i < rowOnePotential.length - 1; i++) {
-      // console.log(rowTwoPotential[i], rowOnePotential[i + 1]);
-      if (rowTwoPotential[i] > rowOnePotential[i + 1]) {
-        moveDownIndex = i;
+      for (let i = 0; i < rowOnePotential.length - 1; i++) {
+        // console.log(rowTwoPotential[i], rowOnePotential[i + 1]);
+        if (rowTwoPotential[i] > rowOnePotential[i + 1]) {
+          moveDownIndex = i;
+        }
       }
-    }
 
-    if (moveDownIndex === -1) {
-      moveDownIndex = firstRow.length - 1;
-    }
+      if (moveDownIndex === -1) {
+        moveDownIndex = firstRow.length - 1;
+      }
 
-    console.log(moveDownIndex);
+      if (overwriteValues) {
+        for (let i = 0; i < firstRow.length; i++) {
+          if (moveDownIndex === i) {
+            firstRow[i] = 0;
+            secondRow[i] = 0;
+          } else if (i < moveDownIndex) {
+            firstRow[i] = 0;
+          } else {
+            secondRow[i] = 0;
+          }
+        }
+      } else {
+        let secondCounter = 0;
+        for (let i = 0; i < firstRow.length; i++) {
+          //  console.log(firstRow[i]);
+          //  console.log(firstRow[i]);
+          if (moveDownIndex === i) {
+            secondCounter += firstRow[i];
+            secondCounter += secondRow[i];
+          } else if (i < moveDownIndex) {
+            secondCounter += firstRow[i];
+          } else {
+            secondCounter += secondRow[i];
+          }
+          //  console.log(secondCounter);
+        }
+        console.log({ secondCounter });
+        //   console.log(secondCounter);
+      }
+
+      // console.log(moveDownIndex);
+    };
+
+    //  console.log(firstRow);
+    //  console.log(secondRow);
+
+    setPotentialArrays();
+
+    smartNavigation(true);
+
+    setPotentialArrays();
+
+    smartNavigation();
+
+    //  console.log(rowOnePotential);
+    //  console.log(rowTwoPotential);
+
+    //  console.log({ firstRow });
+    //  console.log({ secondRow });
 
     //  console.log(grid);
 
@@ -49,14 +97,14 @@ function gridGame(grid: number[][]): number {
   return optimumPath;
 }
 
-// gridGame([
-//   [2, 5, 4],
-//   [1, 5, 1],
-// ]);
-// gridGame([
-//   [3, 3, 1],
-//   [8, 5, 2],
-// ]);
+gridGame([
+  [2, 5, 4],
+  [1, 5, 1],
+]);
+gridGame([
+  [3, 3, 1],
+  [8, 5, 2],
+]);
 gridGame([
   [1, 3, 1, 15],
   [1, 3, 3, 1],
