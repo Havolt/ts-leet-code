617cb92ee5d9380186f426177cc6c5693dd7a3d3
Mark <markfitz815@gmail.com>
2025-06-16 18:09:09 +0100

    Get moveDownPosition

--- src/robot-game.ts
+++ src/robot-game.ts
@@ -1,1 +1,63 @@
-console.log("Hello World");
+function gridGame(grid: number[][]): number {
+  // Can only move right or down
+  const getOptimumPath = (grid): number => {
+    const firstRow = [...grid[0]];
+    const secondRow = [...grid[1]];
+
+    const rowOnePotential: number[] = [];
+    const rowTwoPotential: number[] = [];
+
+    let counter = 0;
+
+    [...firstRow].reverse().forEach((value) => {
+      counter += value;
+      rowOnePotential.unshift(counter);
+    });
+
+    counter = 0;
+
+    [...secondRow].reverse().forEach((value) => {
+      counter += value;
+      rowTwoPotential.unshift(counter);
+    });
+
+    console.log(rowOnePotential);
+    console.log(rowTwoPotential);
+
+    let moveDownIndex = -1;
+
+    for (let i = 0; i < rowOnePotential.length - 1; i++) {
+      // console.log(rowTwoPotential[i], rowOnePotential[i + 1]);
+      if (rowTwoPotential[i] > rowOnePotential[i + 1]) {
+        moveDownIndex = i;
+      }
+    }
+
+    if (moveDownIndex === -1) {
+      moveDownIndex = firstRow.length - 1;
+    }
+
+    console.log(moveDownIndex);
+
+    //  console.log(grid);
+
+    //  firstRow.reduce();
+    return 0;
+  };
+
+  const optimumPath = getOptimumPath(grid);
+  return optimumPath;
+}
+
+// gridGame([
+//   [2, 5, 4],
+//   [1, 5, 1],
+// ]);
+// gridGame([
+//   [3, 3, 1],
+//   [8, 5, 2],
+// ]);
+gridGame([
+  [1, 3, 1, 15],
+  [1, 3, 3, 1],
+]);
