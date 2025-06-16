fe1af063a64946357baab57745436f5db25e3395
Mark <markfitz815@gmail.com>
2025-06-16 21:56:47 +0100

    Remove old code

--- src/robot-game.ts
+++ src/robot-game.ts
@@ -1,117 +1,16 @@
 function gridGame(grid: number[][]): number {
-  // Can only move right or down
-  const getOptimumPath = (grid): number => {
-    const firstRow: number[] = [...grid[0]];
-    const secondRow: number[] = [...grid[1]];
+  const firstRow = grid[0];
+  const secondRow = grid[1];
 
-    const rowOnePotential: number[] = [];
-    const rowTwoPotential: number[] = [];
-
-    const setPotentialArrays = () => {
-      let counter = secondRow[secondRow.length - 1];
-      rowOnePotential.splice(0);
-      rowTwoPotential.splice(0);
-
-      [...firstRow].reverse().forEach((value) => {
-        counter += value;
-        rowOnePotential.unshift(counter);
-      });
-
-      counter = 0;
-
-      [...secondRow].reverse().forEach((value) => {
-        counter += value;
-        rowTwoPotential.unshift(counter);
-      });
-    };
-
-    const smartNavigation = (overwriteValues = false) => {
-      let moveDownIndex = -1;
-
-      for (let i = 0; i < rowOnePotential.length - 1; i++) {
-        console.log(rowTwoPotential[i], rowOnePotential[i + 1]);
-        if (rowTwoPotential[i] > rowOnePotential[i + 1]) {
-          moveDownIndex = i;
-          break;
-        }
-      }
-
-      if (moveDownIndex === -1) {
-        moveDownIndex = firstRow.length - 1;
-      }
-
-      if (overwriteValues) {
-        for (let i = 0; i < firstRow.length; i++) {
-          if (moveDownIndex === i) {
-            firstRow[i] = 0;
-            secondRow[i] = 0;
-          } else if (i < moveDownIndex) {
-            firstRow[i] = 0;
-          } else {
-            secondRow[i] = 0;
-          }
-        }
-      } else {
-        let secondCounter = 0;
-        for (let i = 0; i < firstRow.length; i++) {
-          //  console.log(firstRow[i]);
-          //  console.log(firstRow[i]);
-          if (moveDownIndex === i) {
-            secondCounter += firstRow[i];
-            secondCounter += secondRow[i];
-          } else if (i < moveDownIndex) {
-            secondCounter += firstRow[i];
-          } else {
-            secondCounter += secondRow[i];
-          }
-          //  console.log(secondCounter);
-        }
-        console.log({ secondCounter });
-        return secondCounter;
-        //   console.log(secondCounter);
-      }
-
-      // console.log(moveDownIndex);
-    };
-
-    //  console.log(firstRow);
-    //  console.log(secondRow);
-
-    setPotentialArrays();
-
-    console.log({ firstRow });
-    console.log({ secondRow });
-
+  // First robot wants to make sure second robot collects as few points as possible
+  // Second robot wants to collect as many remaining points as possible
+  for (let i = 0; i < firstRow.length; i++) {
+    console.log(firstRow[i]);
+    console.log(secondRow[i]);
     console.log("---");
+  }
 
-    console.log({ rowOnePotential });
-    console.log({ rowTwoPotential });
-
-    smartNavigation(true);
-
-    setPotentialArrays();
-
-    console.log("----------------");
-
-    console.log({ firstRow });
-    console.log({ secondRow });
-
-    console.log("---");
-
-    console.log({ rowOnePotential });
-    console.log({ rowTwoPotential });
-
-    //  console.log({ firstRow });
-    //  console.log({ secondRow });
-
-    //  console.log(grid);
-
-    //  firstRow.reduce();
-    return smartNavigation() || 0;
-  };
-
-  const optimumPath = getOptimumPath(grid);
-  return optimumPath;
+  return 0;
 }
 
 // gridGame([
