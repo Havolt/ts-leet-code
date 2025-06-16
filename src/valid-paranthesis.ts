d1765ee6454fcba14ee6f762feca40111b0569d8
Mark <markfitz815@gmail.com>
2025-06-16 22:54:35 +0100

    Working valid-paranthesis

--- src/valid-paranthesis.ts
+++ src/valid-paranthesis.ts
@@ -6,30 +6,36 @@ function isValid(s: string): boolean {
 
   const splitString = s.split("");
 
-  let mismatchFound = false;
-
   // Can't be odd if we're matching
   if (splitString.length % 2 !== 0) return false;
 
-  //  Match first half to second half
-  for (let i = 0; i < splitString.length / 2; i++) {
-    const openingBracket = splitString[i];
-    const closingBracket = splitString[splitString.length - 1 - i];
-    if (
-      (openingBracket === "(" && closingBracket !== ")") ||
-      (openingBracket === "{" && closingBracket !== "}") ||
-      (openingBracket === "[" && closingBracket !== "]")
-    ) {
-      mismatchFound = true;
-      break;
+  //   Use left as key and right as val
+  const paranthesis = {
+    "{": "}",
+    "(": ")",
+    "[": "]",
+  };
+
+  //   Store unmatched paranthesis
+  const pChest: string[] = [];
+
+  //   Check the whole array
+  for (let i = 0; i < splitString.length; i++) {
+    if (i === 0) {
+      pChest.push(splitString[i]);
+    } else if (splitString[i] === paranthesis[pChest[pChest.length - 1]]) {
+      pChest.pop();
+    } else {
+      pChest.push(splitString[i]);
     }
   }
 
-  console.log(s);
-  console.log(!mismatchFound);
-  console.log("----");
+  //   We need to look through whole array
+  // When we get an opening bracket we need to find closing bracket (if it exists)
+  // If the next item is not a closing bracket then we need to check for its closing bracket next
+  // Once a closing bracket is found then can mark it done
 
-  return !mismatchFound;
+  return pChest.length === 0;
 }
 
 isValid("()");
