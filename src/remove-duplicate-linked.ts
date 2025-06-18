b8b85f77e6f7b1191638545079cadc1032d79be9
Mark <markfitz815@gmail.com>
2025-06-18 22:43:25 +0100

    Complete remove-duplicate-linked

--- src/remove-duplicate-linked.ts
+++ src/remove-duplicate-linked.ts
@@ -40,6 +40,21 @@ class LinkedList {
       this.size++;
     }
   }
+
+  getFlatArray(): number[] {
+    if (this.head === null) {
+      return [];
+    }
+    const flattenedArray: number[] = [];
+    let currentNode = this.head;
+    flattenedArray.push(currentNode.val);
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      flattenedArray.push(currentNode.val);
+    }
+
+    return flattenedArray;
+  }
 }
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
@@ -48,18 +63,33 @@ function deleteDuplicates(head: ListNode | null): ListNode | null {
     return null;
   }
 
-  const currentNode = head;
-  //   We need to look at the next node and see its value
+  let currentNode = head;
+
+  const cleanedLinkedList = new LinkedList();
+  cleanedLinkedList.add(currentNode.val);
+
+  let currentValue: number | null = currentNode.val;
+  // We need to look at the next node and see its value
   // If it's the same then we need to peek at the following node's next
   // If it's the same then repeat, otherwise set the
   // currentNode to link directly to the next different value
-  while (currentNode.next) {}
-  return null;
+  while (currentNode.next) {
+    currentNode = currentNode.next;
+    if (currentNode.val !== currentValue) {
+      cleanedLinkedList.add(currentNode.val);
+      currentValue = currentNode.val;
+    }
+  }
+  return cleanedLinkedList.head;
 }
 
 const linkedListA = new LinkedList();
 linkedListA.add(1);
 linkedListA.add(1);
 linkedListA.add(2);
+linkedListA.add(2);
+linkedListA.add(3);
+linkedListA.add(4);
+linkedListA.add(7);
 
 console.log(deleteDuplicates(linkedListA.head));
