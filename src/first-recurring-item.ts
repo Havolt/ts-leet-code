771dc49b3156c7946c5e908efa60310bd4a06137
Mark <markfitz815@gmail.com>
2025-07-03 23:12:33 +0100

    First recurring item

---
+++ src/first-recurring-item.ts
@@ -0,0 +1,14 @@
+function firstRecurringItem(numberArr: number[]): number | undefined {
+  const seenNums = new Set();
+  for (const number of numberArr) {
+    if (seenNums.has(number)) {
+      return number;
+    }
+    seenNums.add(number);
+  }
+  return undefined;
+}
+
+console.log(firstRecurringItem([2, 4, 7, 1, 2, 8, 4]));
+console.log(firstRecurringItem([4, 7, 8]));
+console.log(firstRecurringItem([]));
