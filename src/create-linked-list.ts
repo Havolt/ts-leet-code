902ee28545801bdbe4f3b64c38c513fdd3e2764a
Mark <markfitz815@gmail.com>
2025-07-06 08:57:25 +0100

    Create linked list insert method

--- src/create-linked-list.ts
+++ src/create-linked-list.ts
@@ -12,18 +12,12 @@ class LinkedList {
   tail: LinkedNode;
   length: number;
   constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-    };
+    this.head = new LinkedNode(value);
     this.tail = this.head;
     this.length = 1;
   }
   append(value) {
-    const newNode: LinkedNode = {
-      value,
-      next: null,
-    };
+    const newNode = new LinkedNode(value);
 
     this.tail.next = newNode;
     this.tail = newNode;
@@ -31,16 +25,38 @@ class LinkedList {
     return this;
   }
   prepend(value) {
-    const newNode: LinkedNode = {
-      value,
-      next: this.head,
-    };
+    const newNode = new LinkedNode(value);
+    newNode.next = this.tail;
 
     this.head = newNode;
     this.length++;
     return this;
   }
-  insert() {}
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+    } else if (index === this.length) {
+      console.log("append ", value);
+      this.append(value);
+    } else if (index > this.length) {
+      console.error("Index too large");
+      return new Error("Index too large");
+    } else {
+      let pointerIndex = 0;
+      let pointer: LinkedNode = this.head;
+      while (pointerIndex !== index - 1 && pointer.next) {
+        pointer = pointer.next;
+        pointerIndex++;
+      }
+      const newNode = new LinkedNode(value);
+
+      const nextNode = pointer.next;
+      pointer.next = newNode;
+      newNode.next = nextNode;
+
+      this.length++;
+    }
+  }
   toArray() {
     let pointer = this.head;
     const linkedArray: number[] = [pointer.value];
@@ -53,10 +69,16 @@ class LinkedList {
 }
 
 let myLinkedList = new LinkedList(1);
-myLinkedList.append(5);
-myLinkedList.append(16);
-myLinkedList.prepend(-2);
-myLinkedList.prepend(-20);
+// myLinkedList.append(5);
+// myLinkedList.append(16);
+// myLinkedList.prepend(-2);
+// myLinkedList.prepend(-20);
+
+myLinkedList.insert(0, -1);
+myLinkedList.insert(2, 2);
+myLinkedList.insert(3, 3);
+myLinkedList.insert(10, 3);
+myLinkedList.insert(2, 10);
 
 console.log(myLinkedList);
 console.log(myLinkedList.toArray());
