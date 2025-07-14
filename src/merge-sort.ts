f493238c42ec7a12aaaf6d2dad6081c7b6966a57
Mark <markfitz815@gmail.com>
2025-07-14 22:43:02 +0100

    Initial setup for merge sort

---
+++ src/merge-sort.ts
@@ -0,0 +1,22 @@
+const numbers: number[] = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(array: number[]): number[] {
+  if (array.length === 1) {
+    return array;
+  }
+  // Split Array in into right and left
+  const left = array.slice(0, Math.floor(array.length / 2));
+  const right = array.slice(Math.floor(array.length / 2));
+
+  console.log(left);
+  console.log(right);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left: number[], right: number[]): number[] {
+  return [];
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
