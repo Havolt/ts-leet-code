22e2ef935464188af54fb9881bf592a857a40b30
Mark <markfitz815@gmail.com>
2025-07-15 22:50:09 +0100

    Add the skeleton for quick sort

---
+++ src/quick-sort.ts
@@ -0,0 +1,16 @@
+const numbers: number[] = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array: number[], left: number, right: number) {}
+
+function partition(
+  array: number[],
+  pivot: number,
+  left: number,
+  right: number
+) {}
+
+function swap(array: number[], firstIndex: number, secondIndex: number) {}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
