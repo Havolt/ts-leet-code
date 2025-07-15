b3ff5e28b4e8ba4ea7f98a2c9bf6a41fe8da3cf7
Mark <markfitz815@gmail.com>
2025-07-15 22:55:48 +0100

    Add in comment with steps for what we need to do

--- src/quick-sort.ts
+++ src/quick-sort.ts
@@ -1,5 +1,10 @@
 const numbers: number[] = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
+// Get the pointer
+// Compare all the numbers to the pointer
+// Find the final place for the pointer
+// Once we have this split the array to the left and right of the pointer final place
+// Repeat above steps for all recursive arrays
 function quickSort(array: number[], left: number, right: number) {}
 
 function partition(
