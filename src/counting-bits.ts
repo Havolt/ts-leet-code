358573e5ee9b8b2cb7a567cce836672bb939cf4a
Mark <markfitz815@gmail.com>
2025-07-20 22:08:41 +0100

    Cleanup counting bits

--- src/counting-bits.ts
+++ src/counting-bits.ts
@@ -1,32 +1,15 @@
 function countBits(n: number): number[] {
-  const binaryRange = [
-    65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8,
-    4, 2, 1,
-  ];
-  const binaryRangeMax = binaryRange.length - 1;
-
-  const checkNumbers = (currentNum, iAmount = 0, binaryRangePos = 0) => {
-    if (currentNum >= binaryRange[binaryRangePos]) {
-      iAmount++;
-      currentNum -= binaryRange[binaryRangePos];
+  const initialArr = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    if (i % 2 === 0) {
+      initialArr.push(initialArr[i / 2]);
+    } else {
+      initialArr.push(initialArr[i - 1] + 1);
     }
-    if (currentNum !== 0 && binaryRangePos + 1 <= binaryRangeMax) {
-      return checkNumbers(currentNum, iAmount, binaryRangePos + 1);
-    }
-
-    return iAmount;
-  };
-
-  const outputArray: number[] = [];
-
-  for (let i = 0; i <= n; i++) {
-    let currentNum = i;
-
-    outputArray.push(checkNumbers(currentNum));
   }
 
-  console.log(outputArray);
-  return outputArray;
+  console.log(initialArr);
+  return initialArr;
 }
 
 countBits(20);
