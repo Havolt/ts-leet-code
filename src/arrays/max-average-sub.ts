95025bc878c053db561e4ac1ad47094987d2152d
Mark <markfitz815@gmail.com>
2025-07-29 22:11:55 +0100

    Speed up find max average

--- src/arrays/max-average-sub.ts
+++ src/arrays/max-average-sub.ts
@@ -1,22 +1,21 @@
 function findMaxAverage(nums: number[], k: number): number {
   let overallPosition = 0;
-  let largestTally: number | null = null;
+  let currentTally = nums.slice(0, k).reduce((a, b) => a + b);
+  let largestAverage: number = currentTally / k;
 
-  while (overallPosition <= nums.length - k) {
-    const currAverage =
-      nums.slice(overallPosition, overallPosition + k).reduce((a, b) => a + b) /
-      k;
-
-    if (largestTally === null || currAverage > largestTally) {
-      largestTally = currAverage;
-    }
-
+  while (overallPosition < nums.length - k) {
+    currentTally -= nums[overallPosition];
     overallPosition++;
+    currentTally += nums[overallPosition + k - 1];
+    const currAverage = currentTally / k;
+    if (currAverage > largestAverage) {
+      largestAverage = currAverage;
+    }
   }
 
-  console.log(largestTally);
+  console.log(largestAverage);
 
-  return largestTally!;
+  return largestAverage;
 }
 
 findMaxAverage([1, 12, -5, -6, 50, 3], 4);
