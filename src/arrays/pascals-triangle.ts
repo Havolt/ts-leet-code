6a8da779f34c7372e143c1a2552f2f144683620c
Mark <markfitz815@gmail.com>
2026-05-01 21:34:49 +0100

    Finish Pascals Triangle

--- src/arrays/pascals-triangle.ts
+++ src/arrays/pascals-triangle.ts
@@ -6,24 +6,16 @@ function generate(numRows: number): number[][] {
     return fullRows;
   }
   fullRows.push([1, 1]);
-  for (let i = 1; i < numRows - 1; i++) {
-    const newRow = [...fullRows[i]];
-    // Even
-    if (i % 2 === 0) {
-      const middlePos = Math.floor(newRow.length / 2);
-      newRow.splice(middlePos + 1, 0, newRow[middlePos]);
-    } // Odd
-    else {
-      const middleLeftPos = newRow.length / 2 - 1;
-      newRow.splice(middleLeftPos + 1, 0, newRow[middleLeftPos] + 1);
+
+  for (let i = 2; i < numRows; i++) {
+    const newRow = [1];
+    const prevRow = fullRows[i - 1];
+    for (let j = 0; j < prevRow.length - 1; j++) {
+      newRow.push(prevRow[j] + prevRow[j + 1]);
     }
-    for (let j = 1; j < newRow.length - 1; j++) {
-      newRow[j]++;
-    }
+    newRow.push(1);
     fullRows.push(newRow);
   }
-
-  console.log(fullRows);
 
   return fullRows;
 }
