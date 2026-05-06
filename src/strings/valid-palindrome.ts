4f9bd273e06761e1e97a65ddd82c6ef205c893b5
Mark <markfitz815@gmail.com>
2026-05-06 21:22:28 +0100

    Improve speed of palindrome

--- src/strings/valid-palindrome.ts
+++ src/strings/valid-palindrome.ts
@@ -1,4 +1,48 @@
 function isPalindrome(s: string): boolean {
+  const split = s.split("");
+  let leftPointer = 0;
+  let rightPointer = split.length - 1;
+
+  const isAlphaNumeric = (character: string) => {
+    const characterCode = character.charCodeAt(0);
+    if (characterCode >= 97 && characterCode <= 122) {
+      return true;
+    }
+    if (!isNaN(+character) && character !== " ") {
+      return true;
+    }
+  };
+
+  while (leftPointer < rightPointer) {
+    const leftCharacter = split[leftPointer];
+    const rightCharacter = split[rightPointer];
+
+    const leftLowerCase = leftCharacter.toLocaleLowerCase();
+
+    if (!isAlphaNumeric(leftLowerCase)) {
+      leftPointer++;
+      continue;
+    }
+
+    const rightLowerCase = rightCharacter.toLocaleLowerCase();
+
+    if (!isAlphaNumeric(rightLowerCase)) {
+      rightPointer--;
+      continue;
+    }
+
+    if (leftLowerCase === rightLowerCase) {
+      leftPointer++;
+      rightPointer--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function _isPalindrome(s: string): boolean {
   const split = s.split("").filter((character) => {
     const characterCode = character.toLocaleLowerCase().charCodeAt(0);
     if (characterCode >= 97 && characterCode <= 122) {
@@ -15,8 +59,6 @@ function isPalindrome(s: string): boolean {
     return true;
   }
 
-  console.log({ split });
-
   while (leftPointer < rightPointer) {
     const leftLetter = split[leftPointer].toLocaleLowerCase();
     const rightLetter = split[rightPointer].toLocaleLowerCase();
@@ -30,7 +72,9 @@ function isPalindrome(s: string): boolean {
   return true;
 }
 
-isPalindrome("A man, a plan, a canal: Panama");
-isPalindrome("!a!");
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("!a!"));
 
-isPalindrome("0vv0");
+console.log(isPalindrome("0vv0"));
+console.log(isPalindrome("0vv0"));
+console.log(isPalindrome("0vv05"));
