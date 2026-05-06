bee74d507deae0df9b8c2e994caaf29001f09571
Mark <markfitz815@gmail.com>
2026-05-06 21:10:57 +0100

    First version of palindrome fix

--- src/strings/valid-palindrome.ts
+++ src/strings/valid-palindrome.ts
@@ -2,22 +2,35 @@ function isPalindrome(s: string): boolean {
   const split = s.split("").filter((character) => {
     const characterCode = character.toLocaleLowerCase().charCodeAt(0);
     if (characterCode >= 97 && characterCode <= 122) {
-      return character.toLocaleLowerCase;
+      return true;
+    }
+    if (!isNaN(+character) && character !== " ") {
+      return true;
     }
   });
-  //   const useMeeting = split.length % 2 !== 0; // Count character where pointers meet?
   let leftPointer = 0;
   let rightPointer = split.length - 1;
 
-  // Pseudo code for later
-  // Check left and right match
-  // Increment/decrement by one each time
-  // If pointers land on same position check it and return
-  // If left pointer goes further than right pointer then dont count this check
+  if (split.length === 0 || split.length === 1) {
+    return true;
+  }
 
-  console.log(split);
+  console.log({ split });
 
+  while (leftPointer < rightPointer) {
+    const leftLetter = split[leftPointer].toLocaleLowerCase();
+    const rightLetter = split[rightPointer].toLocaleLowerCase();
+    if (leftLetter === rightLetter) {
+      leftPointer++;
+      rightPointer--;
+    } else {
+      return false;
+    }
+  }
   return true;
 }
 
 isPalindrome("A man, a plan, a canal: Panama");
+isPalindrome("!a!");
+
+isPalindrome("0vv0");
